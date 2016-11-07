c960ece738c7e1b0be273d91b0b79aa9c0451f03
Aviad <aviadkatani@gmail.com>
2016-11-07 10:57:37 +0200

    Add 3 Activities.

--- app/src/main/java/aviadapps/equationv2/MainActivity.java
+++ app/src/main/java/aviadapps/equationv2/MainActivity.java
@@ -1,13 +1,104 @@
 package aviadapps.equationv2;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+    Random rnd;
+    Intent getIT;
+    double a,b,c;
+    EditText aEV, bEV, cEV;
+    boolean click;
+    boolean isReturned;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        aEV = (EditText)findViewById(R.id.aTV);
+        bEV = (EditText)findViewById(R.id.bTV);
+        cEV = (EditText)findViewById(R.id.cTV);
+
+        getIT = getIntent();
+        isReturned = getIT.getBooleanExtra("sign", true);
+        // If the main activity is returned (goes back from another activity)
+        if(isReturned) {
+            a = getIT.getDoubleExtra("a", 0);
+            b = getIT.getDoubleExtra("b", 0);
+            c = getIT.getDoubleExtra("c", 0);
+            aEV.setText("" + a);
+            bEV.setText("" + b);
+            cEV.setText("" + c);
+        }
+    }
+    // Move to credits activity while saving the data
+    public void CreditsClicked(View view) {
+        isReturned = true;
+        Intent cIntent = new Intent(this, CreditsActivity.class);
+        cIntent.putExtra("a", a);
+        cIntent.putExtra("b", b);
+        cIntent.putExtra("c", c);
+        cIntent.putExtra("sign", true);
+        startActivity(cIntent);
+    }
+    // Move to the solution activity while saving the data
+    public void SolutionClicked(View view) {
+        isReturned = true;
+        Intent sIntent = new Intent(this, SolutionActivity.class);
+        sIntent.putExtra("a", a);
+        sIntent.putExtra("b", b);
+        sIntent.putExtra("c", c);
+        startActivity(sIntent);
+    }
+    // Generate numbers between -10 to 10
+    public void GenerateClicked(View view) {
+        rnd = new Random();
+        a = rnd.nextInt(21) - 10;
+        b = rnd.nextInt(21) - 10;
+        c = rnd.nextInt(21) - 10;
+        aEV.setText("" + a);
+        bEV.setText("" + b);
+        cEV.setText("" + c);
+        // Generate 3 numbers to 3 EV's.
+    }
+
+    // When clicked,  we'll try to take the numbers from the input and go to solution activity
+    public void CalculateClicked(View view) {
+        // If nothing goes wrong and the app can do try { .. } without crash
+        try {
+            a = Double.parseDouble(aEV.getText().toString());
+            b = Double.parseDouble(bEV.getText().toString());
+            c = Double.parseDouble(cEV.getText().toString());
+            // If a is equal to 0, the equation cannot be solved (no solution)
+            if(a == 0) {
+                Toast.makeText(this, "A can't be 0" + a, Toast.LENGTH_LONG).show();
+            }
+            // If the equation can be solved, then the data goes to Solution Activity
+            else {
+                click = true;
+                isReturned = true;
+                Intent sIntent = new Intent(this, SolutionActivity.class);
+                sIntent.putExtra("a", a);
+                sIntent.putExtra("b", b);
+                sIntent.putExtra("c", c);
+                startActivity(sIntent);
+            }
+        }
+        // If something is wrong... (Most of the times, there aren't inputs which cause to a crash
+        catch (Exception e) {
+            aEV.setError("You have to choose a number or click the Generate button.");
+            bEV.setError("You have to choose a number or click the Generate button.");
+            cEV.setError("You have to choose a number or click the Generate button.");
+            click = false;
+        }
     }
 }
+
---
+++ app/src/main/java/aviadapps/equationv2/SolutionActivity.java
@@ -0,0 +1,106 @@
+package aviadapps.equationv2;
+
+import android.content.Intent;
+import android.media.Image;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+        import java.text.DecimalFormat;
+
+public class SolutionActivity extends AppCompatActivity {
+    Intent getIntent;
+    double a, b, c;
+    TextView validTV, solutionTV, solution2TV, aTV, bTV, cTV;
+    boolean isValid;
+    double solution, solution2, calc;
+    ImageView resultView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_solution);
+
+        validTV = (TextView) findViewById(R.id.validTV);
+        solutionTV = (TextView) findViewById(R.id.solutionTV);
+        solution2TV = (TextView) findViewById(R.id.solution2TV);
+        aTV = (TextView)findViewById(R.id.aTV);
+        bTV = (TextView)findViewById(R.id.bTV);
+        cTV = (TextView)findViewById(R.id.cTV);
+        resultView = (ImageView)findViewById(R.id.resultView);
+
+        getIntent = getIntent();
+        a = getIntent.getDoubleExtra("a", 0);
+        b = getIntent.getDoubleExtra("b", 0);
+        c = getIntent.getDoubleExtra("c", 0);
+        // If the user didn't put anything
+        if ((a == 0) && (b == 0) && (c == 0)) {
+            validTV.setText("Choose numbers on the main tab.");
+            solutionTV.setText("Choose numbers on the main tab.");
+            solution2TV.setText("Choose numbers on the main tab.");
+            // If the user did put something in
+        } else {
+            calc = b * b - 4 * a * c;
+            // If the numbers in the square aren't equal to 0 => because you won't be able to square it
+            if (0 <= calc) {
+                isValid = true;
+                validTV.setText("The solution is valid!");
+                aTV.setText("a: " + a);
+                bTV.setText("b: " + b);
+                cTV.setText("c: " + c);
+                // The numbers in the square are 0 <=
+            } else {
+                isValid = false;
+                validTV.setText("The solution isn't valid!");
+                solutionTV.setText("You can't square a number below 0");
+                solution2TV.setText("You can't square a number below 0");
+            }
+
+            // If the numbers can be calculated to a minimum 0ne solution
+            if (isValid) {
+                DecimalFormat df = new DecimalFormat("#.##");
+                // If true, then there is only one solution
+                solution = -b / (2 * a);
+                solution = Double.valueOf(df.format(solution));
+                solutionTV.setText("x1: " + solution);
+                solution2TV.setText("There is no x2.");
+            }
+                    // Where to assign the image.
+                // calc != 0, so there is 2 solutions
+            else {
+                DecimalFormat df = new DecimalFormat("#.##");
+                solution = (-b + Math.sqrt(calc)) / (2 * a);
+                solution = Double.valueOf(df.format(solution));
+                solution2 = (-b - Math.sqrt(calc)) / (2 * a);
+                solution2 = Double.valueOf(df.format(solution2));
+                solutionTV.setText("x1: " + solution);
+                solution2TV.setText("x2: " + solution2);
+                // Where to assign the Image
+            }
+
+        }
+    }
+
+    // Move to credits activity while saving the data
+    public void CreditsClicked(View view) {
+        Intent cIntent = new Intent(this, CreditsActivity.class);
+        cIntent.putExtra("a", a);
+        cIntent.putExtra("b", b);
+        cIntent.putExtra("c", c);
+        cIntent.putExtra("sign", true);
+        startActivity(cIntent);
+    }
+    // Move to main activity while saving the data
+    public void MainClicked(View view) {
+        Intent mIntent = new Intent(this, MainActivity.class);
+        if (isValid) {
+            mIntent.putExtra("a", a);
+            mIntent.putExtra("b", b);
+            mIntent.putExtra("c", c);
+            mIntent.putExtra("sign", isValid);
+        }
+        startActivity(mIntent);
+    }
+}
